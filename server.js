51c67ce245d02760b97039085c496e3f866375b3
DJ Zevenbergen <djzevenbergen@gmail.com>
2020-11-11 15:53:33 -0800

    deploy

--- server.js
+++ server.js
@@ -35,20 +35,6 @@ app.listen(port, function (err) {
   }
 });
 
-// const http = require('http');
-
-// const hostname = '127.0.0.1';
-
-// const server = http.createServer((req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/plain');
-//   res.end('Node API is running!!');
-// });
-
-// server.listen(port, hostname, () => {
-//   console.log(`Server running at http://${hostname}:${port}/`);
-// });
-
 
 console.log('server running');
 
